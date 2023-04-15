f002c14a2544786025fd5b826e77d8a4cf437fdb
Luis Felipe <luis.fhb.2016@gmail.com>
2023-04-15 09:15:06 -0500

    session 1

---
+++ app/Http/Controllers/FacturaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FacturaController extends Controller
+{
+    public function index(){
+        echo '<h1>Bienvenido a Integración de Aplicaciones ISIL 202310</h1>';
+    }
+}
